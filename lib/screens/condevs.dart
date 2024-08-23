da9cb63abd8136f5737b31a9e350eb1f0cb05412
MoryaNischay <nischaymorya@gmail.com>
2024-08-23 10:17:25 +0530

    Both Devices connected

--- lib/screens/condevs.dart
+++ lib/screens/condevs.dart
@@ -9,8 +9,17 @@ class ConDevsPage extends StatefulWidget {
 }
 
 class _ConDevsPageState extends State<ConDevsPage> {
-  final Stream<DocumentSnapshot<Map<String, dynamic>>> _deviceStream =
-      FirebaseFirestore.instance.collection('Image-Stat').doc('Air-Quality').snapshots();
+  final Stream<DocumentSnapshot<Map<String, dynamic>>> _airQualityStream =
+      FirebaseFirestore.instance
+          .collection('Image-Stat')
+          .doc('Air-Quality')
+          .snapshots();
+
+  final Stream<DocumentSnapshot<Map<String, dynamic>>> _imageDetectStream =
+      FirebaseFirestore.instance
+          .collection('Image-Stat')
+          .doc('Image-Detect')
+          .snapshots();
 
   @override
   Widget build(BuildContext context) {
@@ -18,32 +27,73 @@ class _ConDevsPageState extends State<ConDevsPage> {
       appBar: AppBar(
         title: const Text('Connected Devices'),
       ),
-      body: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-        stream: _deviceStream,
-        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('Error: Something went wrong'));
-          }
+      body: Column(
+        children: [
+          StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+            stream: _airQualityStream,
+            builder: (BuildContext context,
+                AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>>
+                    airQualitySnapshot) {
+              if (airQualitySnapshot.hasError) {
+                return const Center(child: Text('Error: Something went wrong'));
+              }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
+              if (airQualitySnapshot.connectionState ==
+                  ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
 
-          final deviceData = snapshot.data!.data() as Map<String, dynamic>;
-          final name = deviceData['Name'];
-          final status = deviceData['Status'];
-          print('Name: $name, Status: $status');
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Name: $name'),
-                const SizedBox(height: 16.0),
-                Text('Status: ${status ? 'Connected' : 'Disconnected'}'),
-              ],
-            ),
-          );
-        },
+              final airQualityData =
+                  airQualitySnapshot.data!.data() as Map<String, dynamic>;
+              final airQualityName = airQualityData['Name'];
+              final airQualityStatus = airQualityData['Status'];
+
+              return ListTile(
+                title: const Text('Air Quality'),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Name: $airQualityName'),
+                    Text(
+                        'Status: ${airQualityStatus ? 'Connected' : 'Disconnected'}'),
+                  ],
+                ),
+              );
+            },
+          ),
+          StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+            stream: _imageDetectStream,
+            builder: (BuildContext context,
+                AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>>
+                    imageDetectSnapshot) {
+              if (imageDetectSnapshot.hasError) {
+                return const Center(child: Text('Error: Something went wrong'));
+              }
+
+              if (imageDetectSnapshot.connectionState ==
+                  ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+
+              final imageDetectData =
+                  imageDetectSnapshot.data!.data() as Map<String, dynamic>;
+              final imageDetectName = imageDetectData['Name'];
+              final imageDetectStatus = imageDetectData['Status'];
+
+              return ListTile(
+                title: const Text('Image Detect'),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Name: $imageDetectName'),
+                    Text(
+                        'Status: ${imageDetectStatus ? 'Connected' : 'Disconnected'}'),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
