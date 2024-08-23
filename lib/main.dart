e39cbfd6f5c5e634a5c4dc8bdffb9a2cad06bd5f
MoryaNischay <nischaymorya@gmail.com>
2024-08-23 10:02:20 +0530

    Working Fetch

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -28,7 +30,7 @@ class MyApp extends StatelessWidget {
             // Navigate to the home screen with user data
             return HomeScreen(user: user);
           } else {
-            return LoginPage();
+            return const LoginPage();
           }
         },
       ),
--- lib/screens/condevs.dart
+++ lib/screens/condevs.dart
@@ -1,14 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ConDevsPage extends StatelessWidget {
+class ConDevsPage extends StatefulWidget {
+  const ConDevsPage({super.key});
+
+  @override
+  _ConDevsPageState createState() => _ConDevsPageState();
+}
+
+class _ConDevsPageState extends State<ConDevsPage> {
+  final Stream<DocumentSnapshot<Map<String, dynamic>>> _deviceStream =
+      FirebaseFirestore.instance.collection('Image-Stat').doc('Air-Quality').snapshots();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Connected Devices'),
       ),
-      body: Center(
-        child: const Text('Hello'),
+      body: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        stream: _deviceStream,
+        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Error: Something went wrong'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final deviceData = snapshot.data!.data() as Map<String, dynamic>;
+          final name = deviceData['Name'];
+          final status = deviceData['Status'];
+          print('Name: $name, Status: $status');
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Name: $name'),
+                const SizedBox(height: 16.0),
+                Text('Status: ${status ? 'Connected' : 'Disconnected'}'),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
